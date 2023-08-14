2de2e920458b1e0ee6a80f5cda6c70d00c972ce6
fadhilpdd <fadhilabdullah15@gmail.com>
2023-08-14 14:11:50 -0700

    Task 0

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def save(self):
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+    def __str__(self):
+        clname = self.__class__.__name__
+        return f"[{clname}] ({self.id}) {self.__dict__}"
