2276dd3ed32adab296d189f3cdc4b8c105735ffa
fadhilpdd <fadhilabdullah15@gmail.com>
2023-08-14 14:25:04 -0700

    Task basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,17 @@ from datetime import datetime
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
 
     def save(self):
         self.updated_at = datetime.today()
