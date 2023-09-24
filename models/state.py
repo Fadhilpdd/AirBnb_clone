58c9aacca475433091100e1525d2fa37f6a41f69
fadhilpdd <fadhilabdullah15@gmail.com>
2023-09-23 21:28:59 -0700

    Task 5

--- models/state.py
+++ models/state.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """Represent a state.
+class State(BaseModel, Base):
+    """ State class or table model """
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
 
-    Attributes:
-        name (str): The name of the state.
-    """
+        @property
+        def cities(self):
 
-    name = ""
+            """returns the list of City instances with the state_id
+                equals the current State_id
+                FileStorage relationship between State and City
+            """
+            from models import storage
+            cities_related = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    cities_related.append(city)
+            return cities_related
